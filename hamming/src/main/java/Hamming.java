8baeaecf9e73ab88277540f9dec5706dc1ca4619
michaelkunc <mjkunc@gmail.com>
2015-12-19 07:51:36 -0600

    initial hamming solution

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,2 +1,15 @@
+
 public class Hamming {
+    public static Integer compute(String strandA, String strandB) {
+        Integer distance = 0;
+        
+        if (strandA.length() != strandB.length())
+        	throw new IllegalArgumentException();
+        
+        for (int i = 0; i < strandA.length(); i++){
+        	if (strandA.charAt(i) != strandB.charAt(i))
+        		distance++;
+        }
+        return distance;
+    } 
 }
--- hamming/src/test/java/HammingTest.java
+++ hamming/src/test/java/HammingTest.java
@@ -10,44 +10,45 @@ public class HammingTest {
         assertThat(Hamming.compute("A", "A"), is(0));
     }
 
-    // @Test
-    // public void testCompleteHammingDistanceOfForSingleNucleotideStrand() {
-    //     assertThat(Hamming.compute("A", "G"), is(1));
-    // }
+     @Test
+     public void testCompleteHammingDistanceOfForSingleNucleotideStrand() {
+         assertThat(Hamming.compute("A", "G"), is(1));
+     }
 
-    // @Test
-    // public void testCompleteHammingDistanceForSmallStrand() {
-    //     assertThat(Hamming.compute("AG", "CT"), is(2));
-    // }
+     @Test
+     public void testCompleteHammingDistanceForSmallStrand() {
+         assertThat(Hamming.compute("AG", "CT"), is(2));
+     }
 
-    // @Test
-    // public void testSmallHammingDistance() {
-    //     assertThat(Hamming.compute("AT", "CT"), is(1));
-    // }
+     @Test
+     public void testSmallHammingDistance() {
+         assertThat(Hamming.compute("AT", "CT"), is(1));
+     }
 
-    // @Test
-    // public void testSmallHammingDistanceInLongerStrand() {
-    //     assertThat(Hamming.compute("GGACG", "GGTCG"), is(1));
-    // }
+     @Test
+     public void testSmallHammingDistanceInLongerStrand() {
+         assertThat(Hamming.compute("GGACG", "GGTCG"), is(1));
+     }
 
-    // @Test(expected = IllegalArgumentException.class)
-    // public void testValidatesFirstStrandNotLonger() {
-    //     Hamming.compute("AAAG", "AAA");
-    // }
+     @Test(expected = IllegalArgumentException.class)
+     public void testValidatesFirstStrandNotLonger() {
+         Hamming.compute("AAAG", "AAA");
+     }
 
-    // @Test(expected = IllegalArgumentException.class)
-    // public void testValidatesOtherStrandNotLonger() {
-    //     Hamming.compute("AAA", "AAAG");
-    // }
+     @Test(expected = IllegalArgumentException.class)
+     public void testValidatesOtherStrandNotLonger() {
+         Hamming.compute("AAA", "AAAG");
+     }
 
-    // @Test
-    // public void testLargeHammingDistance() {
-    //     assertThat(Hamming.compute("GATACA", "GCATAA"), is(4));
-    // }
+     @Test
+     public void testLargeHammingDistance() {
+         assertThat(Hamming.compute("GATACA", "GCATAA"), is(4));
+     }
 
-    // @Test
-    // public void testHammingDistanceInVeryLongStrand() {
-    //     assertThat(Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT"), is(9));
-    // }
+     @Test
+     public void testHammingDistanceInVeryLongStrand() {
+         assertThat(Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT"), is(9));
+     }
 
 }
+//
