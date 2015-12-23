9e5e15d495c04414ef6204c14274c3fb3a798eca
michaelkunc <mjkunc@gmail.com>
2015-12-23 14:45:21 -0500

    initial nucleotide solution

--- nucleotide-count/src/main/java/DNA.java
+++ nucleotide-count/src/main/java/DNA.java
@@ -1,11 +1,39 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.lang.IllegalArgumentException;
 
 public class DNA {
-	private final String sequence;
+	String sequence;
+	String nucleotides = "ACGT";
 	
 	public DNA(String sequence){
 		this.sequence = sequence;
 	}
 	
+	public int count(char character){
+		int count = 0;
+		if (nucleotides.indexOf(character) == -1) {
+			throw new IllegalArgumentException("You must enter a valid nucleotide.");
+		}
+		
+		for (char x : sequence.toCharArray()) {
+			if ( x == character) {
+				count += 1;
+			}
+		}
+		return count;
+	}
+	
+	public Map<Character, Integer> nucleotideCounts(){
+		Map<Character, Integer> results = new HashMap<>();
+		
+		for (char y : nucleotides.toCharArray()){
+			results.put(y, count(y));
+		}		
+		return results;
+	}
+	
 }
+
+
+
