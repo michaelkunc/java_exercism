dacb783f4c83eb0179e49bdaefd569c35eacc35f
michaelkunc <mjkunc@gmail.com>
2015-12-26 10:13:54 -0500

    slight revision to etl solution

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -3,14 +3,14 @@ import java.util.List;
 import java.util.Map;
 
 public class Etl {
-   public Map<String, Integer> transform(Map<Integer, List<String>> old) {
-	   Map<String, Integer> result = new HashMap<String, Integer>();
-	   for (Map.Entry<Integer, List<String>> entry: old.entrySet()) {
+   public static Map<String, Integer> transform(Map<Integer, List<String>> old) {
+	   Map<String, Integer> result = new HashMap<>();
+	   for (Map.Entry<Integer, List<String>> entry: old.entrySet()){
 		   Integer key = entry.getKey();
 		   for (String e: entry.getValue()){
 			   result.put(e.toLowerCase(), key);
 		   }
 	   }
-	return result;
+	   return result;
    }
 }
