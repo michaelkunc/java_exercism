51f9d4b0a8b4c9ef86070a707bb6f75940d51405
michaelkunc <mjkunc@gmail.com>
2015-12-29 13:58:59 -0600

    word count solution

--- word-count/src/main/java/WordCount.java
+++ word-count/src/main/java/WordCount.java
@@ -4,11 +4,13 @@ import java.util.Map;
 public class WordCount{
     public Map<String, Integer> phrase(String words){
         Map<String, Integer> wordCounts = new HashMap<>();
-        String[] wordArray = words.split("\\s+");
-        for (String word : wordArray) {
+        String[] wordArray = words.toLowerCase().replaceAll("[^a-z0-9 ]","").split("\\s+");
+        for (String word : wordArray){
             Integer count = wordCounts.containsKey(word) ? wordCounts.get(word) : 0;
-            wordCounts.put(word, count += 1);
+            wordCounts.put(word, count +=1 );
         }
         return wordCounts;
     }
 }
+
+
