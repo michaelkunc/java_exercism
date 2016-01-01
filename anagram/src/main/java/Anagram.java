4b54132173f66bee67109fb36cf512a0638350a8
michaelkunc <mjkunc@gmail.com>
2016-01-01 13:35:09 -0600

    anagram solution

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -1,31 +1,30 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 
 public class Anagram {
-
     private String givenWord;
 
-    public Anagram(String givenWord){
+    public Anagram(String givenWord) {
         this.givenWord = givenWord;
     }
 
-    public List<String> match(List<String> possibleMatches){
+    public List<String> match(List<String>possibleMatches){
         List<String> anagrams = new ArrayList<String>();
-        char[] givenWordLetters = this.givenWord.toCharArray();
+        char[] givenWordLetters = this.givenWord.toLowerCase().toCharArray();
         Arrays.sort(givenWordLetters);
 
         for (String possibleMatch : possibleMatches) {
-            char[] possibleMatchLetters = possibleMatch.toCharArray();
+            char[] possibleMatchLetters = possibleMatch.toLowerCase().toCharArray();
             Arrays.sort(possibleMatchLetters);
 
             boolean sameLetters = Arrays.equals(givenWordLetters, possibleMatchLetters);
+            boolean sameWord = this.givenWord.toLowerCase().equals(possibleMatch.toLowerCase());
 
-            if (sameLetters) {
+            if (sameLetters & !sameWord){
                 anagrams.add(possibleMatch);
             }
         }
-        return anagrams;
-
+    return anagrams;
     }
 }
