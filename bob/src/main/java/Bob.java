14d6c8591e87fa28e6c42c79a34e926d09dfd1a3
michaelkunc <mjkunc@gmail.com>
2016-01-02 22:41:31 -0600

    very start of bob solution

---
+++ bob/src/main/java/Bob.java
@@ -0,0 +1,10 @@
+
+public class Bob {
+    private String statementToBob;
+
+    public String hey(String statementToBob ){
+        String response = new String("This is a string");
+
+        return response;
+    }
+}
