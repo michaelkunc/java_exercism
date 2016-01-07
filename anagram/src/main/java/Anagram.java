569643f51c978b31be84ef1f80854634cae1b3fa
michaelkunc <mjkunc@gmail.com>
2016-01-06 18:55:13 -0600

    revised anagram solution

--- anagram/src/main/java/Anagram.java
+++ anagram/src/main/java/Anagram.java
@@ -9,22 +9,28 @@ public class Anagram {
         this.givenWord = givenWord;
     }
 
+    private char[] charArray(String word){
+        char[] charArray = word.toLowerCase().toCharArray();
+        return charArray;
+    }
+
     public List<String> match(List<String>possibleMatches){
         List<String> anagrams = new ArrayList<String>();
-        char[] givenWordLetters = this.givenWord.toLowerCase().toCharArray();
+        char[] givenWordLetters = charArray(this.givenWord);
         Arrays.sort(givenWordLetters);
 
         for (String possibleMatch : possibleMatches) {
-            char[] possibleMatchLetters = possibleMatch.toLowerCase().toCharArray();
+            char[] possibleMatchLetters = charArray(possibleMatch);
             Arrays.sort(possibleMatchLetters);
 
             boolean sameLetters = Arrays.equals(givenWordLetters, possibleMatchLetters);
             boolean sameWord = this.givenWord.toLowerCase().equals(possibleMatch.toLowerCase());
 
-            if (sameLetters & !sameWord){
+            if (sameLetters && !sameWord){
                 anagrams.add(possibleMatch);
             }
         }
     return anagrams;
     }
+
 }
