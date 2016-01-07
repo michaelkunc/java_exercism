ddf02a545308d27e97e6a63b4bd65191976f92be
michaelkunc <mjkunc@gmail.com>
2016-01-07 16:10:56 -0600

    first two tests for bob

--- bob/src/main/java/Bob.java
+++ bob/src/main/java/Bob.java
@@ -2,9 +2,14 @@
 public class Bob {
     private String statementToBob;
 
-    public String hey(String statementToBob ){
-        String response = new String("This is a string");
-
+    public String hey(String statementToBob ) {
+        String response = new String("Whatever.");
+        String lastCharacter = statementToBob.substring(statementToBob.length() - 1);
+        String exclamationPoint = String.valueOf('!');
+        if (lastCharacter.equals(exclamationPoint)){
+            response = "Whoa, chill out!";
+            return response;
+        }
         return response;
     }
 }
