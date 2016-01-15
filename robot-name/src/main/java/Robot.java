bcdd8db54d4f0ebaeb7b877dd2038ab73e90eabf
michaelkunc <mjkunc@gmail.com>
2016-01-15 14:49:45 -0600

    robot name in process

--- robot-name/src/main/java/Robot.java
+++ robot-name/src/main/java/Robot.java
@@ -1,10 +1,15 @@
 import java.util.Random;
+import java.util.Set;
+import java.util.HashSet;
+
 
 public class Robot {
     String name;
+    private static Set<String> namesCurrentlyUsed = new HashSet<String>();
+
 
     public Robot(){
-        name = createName();
+        reset();
     }
 
     public String getName(){
@@ -12,17 +17,29 @@ public class Robot {
     }
 
     public String createName(){
-        String name = String.valueOf(new StringBuilder().append('A').append('B').append(randomNumber()));
+        String name = String.valueOf(new StringBuilder().append(randomLetter()).append(randomLetter()).append(randomNumber()));
         return name;
     }
 
     public void reset(){
-        this.name = "";
+        String oldName = this.name;
+        while (this.name.isEmpty() || namesCurrentlyUsed.contains(this.name)){
+            this.name = createName();
+        }
+        namesCurrentlyUsed.add(this.name);
+        namesCurrentlyUsed.remove(oldName);
     }
-    public static int randomNumber(){
+
+    private static int randomNumber(){
         Random rnd = new Random();
         int randomNumber = rnd.nextInt((999 - 100) + 1) + 0;
         return randomNumber;
+    }
+
+    private static String randomLetter(){
+        Random rnd = new Random();
+        char c = (char)(rnd.nextInt(26) + 'A');
+        return String.valueOf(c);
     }
 
 }
