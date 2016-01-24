e92db24dbc96873dc50d55aa32f62d930d2a1ae3
michaelkunc <mjkunc@gmail.com>
2016-01-23 18:13:33 -0600

    initial meetup solution.

--- meetup/src/main/java/Meetup.java
+++ meetup/src/main/java/Meetup.java
@@ -10,19 +10,40 @@ public class Meetup {
     }
 
     public DateTime day(int weekday, MeetupSchedule schedule){
-        DateTime day;
+        DateTime day = null;
         switch(schedule){
             case FIRST:
-                day =
-
+                day = findDay(weekday, 1, 7, 1);
+                break;
+            case SECOND:
+                day = findDay(weekday, 8, 14, 1);
+                break;
+            case THIRD:
+                day = findDay(weekday, 15, 21, 1);
+                break;
+            case FOURTH:
+                day = findDay(weekday, 22, 28, 1);
+                break;
+            case LAST:
+                int lastDay = new DateTime(year, month,1, 0,0).dayOfMonth().getMaximumValue();
+                day = findDay(weekday, lastDay, lastDay - 7, -1);
+                break;
+            case TEENTH:
+                day = findDay(weekday, 13, 19, 1);
+                break;
         }
-
-
         return day;
 
     }
 
-    private DateTime findDay(int weekday, int start, int stop, int step){
-        DateTime day = new DateTime(year,month, 0,0);
+
+
+    private DateTime findDay(int weekday, int start, int stop, int step) {
+        DateTime day = new DateTime(year, month, start, 0, 0);
+        for (; day.getDayOfMonth() != stop + step; day = day.plusDays(step)) {
+            if (day.getDayOfWeek() == weekday)
+                break;
+        }
+        return day;
     }
 }
