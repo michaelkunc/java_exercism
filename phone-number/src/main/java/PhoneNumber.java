00b3985a083fe5f33b61fe6cb113f538d4060452
michaelkunc <mjkunc@gmail.com>
2016-01-25 16:03:24 -0600

    Initial method setup complete.

---
+++ phone-number/src/main/java/PhoneNumber.java
@@ -0,0 +1,22 @@
+public class PhoneNumber {
+    String inputNumber;
+
+    public PhoneNumber (String inputNumber){
+        this.inputNumber = inputNumber;
+    }
+
+    public String getNumber(){
+        String outputNumber = new String("outputNumber");
+        return outputNumber;
+    }
+
+    public String getAreaCode(){
+        String areaCode = new String("areaCode");
+        return areaCode;
+    }
+
+    public String pretty(){
+        String pretty = new String("Prettyfy");
+        return pretty;
+    }
+}
