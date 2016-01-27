93262a63a2fb33ad1c6e8fc1e7e9eb4e90685b07
michaelkunc <mjkunc@gmail.com>
2016-01-27 08:05:45 -0600

    Two more tests passing.

--- phone-number/src/main/java/PhoneNumber.java
+++ phone-number/src/main/java/PhoneNumber.java
@@ -7,6 +7,7 @@ public class PhoneNumber {
         this.inputNumber = inputNumber;
     }
 
+//not crazy about this particular flow. seems like it could be split out more
     public String getNumber(){
         for (char y : this.inputNumber.toCharArray()) {
             if (Character.isDigit(y)){
@@ -15,26 +16,31 @@ public class PhoneNumber {
         }
         if (isValidLength(outputNumber)){
             return outputNumber;
-        } else {
+        } else if (startsWith11(outputNumber)){
+            return outputNumber.substring(1,11);
+        }
+        else {
             return zeroes;
         }
     }
 
     public String getAreaCode(){
-        String areaCode = new String("areaCode");
+        String areaCode = inputNumber.substring(0,3);
         return areaCode;
     }
 
     public String pretty(){
-        String pretty = new String("Prettyfy");
+        String pretty = "(".concat(inputNumber.substring(0,3)).concat(")").concat(" ").concat(inputNumber.substring(3,6).concat("-").concat(inputNumber.substring(6,10)));
         return pretty;
     }
 
-    private boolean isValidLength(String number){
-        return number.length() == 10;
+    private boolean isValidLength(String phoneNumber){
+        return (phoneNumber.length() == 10);
     }
 
-//    private boolean startsWith11(String number){
-//        return
-//    }
+//    not crazy about this method name
+    private boolean startsWith11(String phoneNumber){
+        return (phoneNumber.length() == 11) && (phoneNumber.substring(0,1) == String.valueOf("1"));
+    }
+
 }
