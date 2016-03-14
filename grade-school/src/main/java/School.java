9c7d32e119a9689fc35b05b315a8563487882e49
michaelkunc <mjkunc@gmail.com>
2016-03-13 19:43:37 -0500

    partial refactor

--- grade-school/src/main/java/School.java
+++ grade-school/src/main/java/School.java
@@ -5,22 +5,28 @@ public class School {
     public Map<Integer, List<String>> school = new HashMap<Integer, List<String>>();
 
     public Map<Integer, List<String>> db(){
-
         return school;
     }
 
     public void add(String studentName, Integer grade){
+        if (school.containsKey(grade)) {
+            List<String> studentGrade = school.get(grade);
+            studentGrade.add(studentName);
+            school.put(grade, studentGrade);
+        } else {
+            List<String> newGrade =
+        }
         List<String> record = grade(grade);
         record.add(studentName);
         school.put(grade, record);
     }
 
-    public List<String> grade(Integer grade){
-        if(school.containsKey(grade)){
-            return school.get(grade);
-        }
-        return new ArrayList<String>();
-    }
+//    public List<String> grade(Integer grade){
+//        if(school.containsKey(grade)){
+//            return school.get(grade);
+//        }
+//        return new ArrayList<String>();
+//    }
 
     public Map<Integer, List<String>> sort() {
         for (Integer i : school.keySet()){
