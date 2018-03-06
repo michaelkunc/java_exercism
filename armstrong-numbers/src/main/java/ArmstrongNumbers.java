2181da7eafc31b191ccc62e92cb5b6fb3bc7725a
michaelkunc <mjkunc@gmail.com>
2018-03-06 06:31:37 -0800

    initial commit of armstrong numbers

---
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,25 @@
+class ArmstrongNumbers {
+
+	boolean armstrongChecker(int numberToCheck) {
+		boolean armstrong = false;
+
+		// will need to split number into their digits
+		// do the math
+		// update the result
+
+		return armstrong;
+	}
+
+	boolean isArmstrongNumber(int numberToCheck) {
+		boolean armstrong = false;
+
+		if (numberToCheck < 10) {
+			armstrong = true;
+		} else if (numberToCheck < 100) {
+			armstrong = false;
+		}
+		return armstrong;
+		
+	}
+
+}
