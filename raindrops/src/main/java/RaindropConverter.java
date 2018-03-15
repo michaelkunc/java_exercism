acb2330f82ad4c039bb24037cc329dd8db161447
Mike Kunc <mike@mediahound.com>
2018-03-15 07:45:28 -0700

    raindrop in progress

--- raindrops/src/main/java/RaindropConverter.java
+++ raindrops/src/main/java/RaindropConverter.java
@@ -2,6 +2,10 @@ class RaindropConverter {
 
     String convert(int number) {
         String strNum = String.valueOf(number);
+        switch (number){
+            case number % 3 == 0:
+            return "Pling";
+        }
         return strNum;
     }
 
