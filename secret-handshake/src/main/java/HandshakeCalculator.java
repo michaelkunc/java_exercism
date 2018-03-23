caa5ac14672cb2496471bd7e2ee8b909192c5a77
michaelkunc <mjkunc@gmail.com>
2018-03-23 08:35:13 -0700

    start of handshake calculator

---
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class HandshakeCalculator {
+
+    List<Signal> calculateHandshake(int number) {
+        List<Signal> result = new ArrayList<Signal>();
+        result.add(Signal.WINK);
+        return result;
+    }
+
+}
+
