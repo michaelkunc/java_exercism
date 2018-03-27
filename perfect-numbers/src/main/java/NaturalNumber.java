bc27aa27e88144a4f2c427cf11e92fb40ce24f25
michaelkunc <mjkunc@gmail.com>
2018-03-27 08:08:46 -0700

    perfect number solution

--- perfect-numbers/src/main/java/NaturalNumber.java
+++ perfect-numbers/src/main/java/NaturalNumber.java
@@ -1,38 +1,43 @@
-import java.util.ArrayList;
-
 class NaturalNumber {
 
     public int testNumber;
 
     public NaturalNumber (int number){
-        testNumber = number;
+        if (number < 1 ){
+            throw new IllegalArgumentException("You must supply a natural number (positive integer)");
+        }
+        this.testNumber = number;
     }
 
-    private int aliquotSum(int testNumber){
+    private int aliquotSum(){
         int sum = 0;
-        int upperLimit = testNumber - 1;
+        int upperLimit = this.testNumber - 1;
         for(int i = 1; i <= upperLimit; i++){
-            if (testNumber % i == 0){
+            if (this.testNumber % i == 0){
                 sum += i;
-                if (i != testNumber/i ){
-                    sum += testNumber/i;
-
-                }
             }
-            return sum;
         }
-
         return sum;
     }
     Classification getClassification() {
-        if (aliquotSum(testNumber) == testNumber){
+        if (aliquotSum() == testNumber){
             return Classification.PERFECT;
+        }
+        if (aliquotSum() > testNumber){
+            return Classification.ABUNDANT;
         }
         else {
             return Classification.DEFICIENT;
         }
 
     }
+
+    // public static void main(String[] args){
+    //     int test = new NaturalNumber(6).aliquotSum();
+    //     System.out.print(test);
+
+
+    // }
 
 
 }
